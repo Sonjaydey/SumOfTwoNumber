435d74822c55baada50ba1a05247cfbc3871941c
Sonjoy <sonjay15-2333@diu.edu.bd>
2021-08-12 21:21:09 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function summationOfTwoNumber(a,b){
+    let sum = a + b;
+    return sum ;
+}
+
+const result1 = summationOfTwoNumber(10,20);
+const result2 = summationOfTwoNumber(30,20);
+const result3 = summationOfTwoNumber(40,20);
+
+console.log(result1,result2,result3);
